dcc2a7c7475eb7fd205ea0acb6b22230f78a32df
Alex Busch <69646582+abusch8@users.noreply.github.com>
2021-04-07 18:24:02 -0500

    updated output.txt

--- com/mgg/SalesReport.java
+++ com/mgg/SalesReport.java
@@ -7,11 +7,6 @@ import java.util.List;
 public class SalesReport {
 
     public static void main(String[] args) {
-//        List<Person> persons = ReadFile.readPersonsCSV();
-//        List<Store> stores = ReadFile.readStoresCSV(persons);
-//        List<Item> items = ReadFile.readItemsCSV();
-//        List<Sale> sales = ReadFile.readSaleCSV(persons, stores, items);
-
         List<Person> persons = LoadData.readPersonDatabase();
         List<Store> stores = LoadData.readStoreDatabase();
         List<Sale> sales = LoadData.readSaleDatabase();
